92fd114689f35cb1168910b15b1e387f2393b164
freesoul.dev <freesoul.dev@gmail.com>
2026-02-18 18:20:12 -0700

    990 cdn

--- components/LandingSection.tsx
+++ components/LandingSection.tsx
@@ -4,9 +4,9 @@ import { useState, useEffect } from 'react'
 import Navigation from '@/components/Navigation'
 
 const videos = [
-  '/images/videos/flowers_water.mp4',
-  '/images/videos/woman_walking.mp4',
-  '/images/videos/woman_water.mp4',
+  'https://990-agency.b-cdn.net/flowers_water.mp4',
+  'https://990-agency.b-cdn.net/woman_walking.mp4',
+  'https://990-agency.b-cdn.net/woman_water.mp4',
 ]
 
 const VIDEO_INDEX_KEY = 'revive_video_index'
